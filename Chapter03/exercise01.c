167b189ab4c6c21134db0c66d4a1bbf65d695759
Meatlf <454779758@qq.com>
2020-08-28 17:30:08 +0800

    feat 习题1和2

---
+++ Chapter03/exercise01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <float.h>
+
+int main()
+{
+    printf("习题1\n");
+    printf("%d\n", SCHAR_MIN);
+    printf("%d\n", SCHAR_MAX);
+
+    printf("习题2\n");
+    printf("%f\n", FLT_MIN);
+    printf("%f\n", FLT_MAX);
+    return EXIT_SUCCESS;
+}  
